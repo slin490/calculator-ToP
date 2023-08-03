54130e35c8c2165bcd1abfbd8b9b32ab7b74389c
Steve <lysoll8444@gmail.com>
2023-08-03 16:51:26 -0500

    prev-screen

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 import { add, subtract, multiply, divide, operate } from './operations.js';
 const currentScreen = document.getElementById("currentScreen");
-const previousScreen = document.getElementById("prevScreen");
+const prevScreen = document.getElementById("prevScreen");
 const displayBtns = document.querySelectorAll(".display");
 const clearBtn = document.querySelector(".clear-btn")
 const equalBtn = document.querySelector(".equal-btn");
 const negativeSign = document.querySelector(".negative-btn");
-const operators = document.querySelectorAll(".operators")
+const operands = document.querySelectorAll(".operators")
 
 displayBtns.forEach((btn) => {
 	btn.addEventListener("click", (event) => {
-		if(currentScreen.textContent.length < 5){
+		if(currentScreen.textContent.length < 5 && prevScreen.textContent.length < 20){
 			currentScreen.textContent += event.target.textContent;
 		}
 	})
 })
 
-operators.forEach((operation) => {
-	operation.addEventListener("click", (event) => {
-		console.log(event.target.textContent);
+operands.forEach((operand) => {
+	operand.addEventListener("click", (event) => {
+		currentScreen.style.marginTop = "15px"
+		prevScreen.style.visibility = "visible";
+		prevScreen.textContent += currentScreen.textContent + " " + event.target.textContent + " ";
+		currentScreen.textContent = ""; 
 	})
 })
 
 const clearScreen = () => {
 	currentScreen.textContent = "";
+	prevScreen.textContent = "";
+	prevScreen.style.visibility= "hidden";
 }
 
 const calculate = () => {
